37046dc05f2c1324e061075b9e8068d417309b72
stupid <stupid.du@coloseo.cn>
2018-03-07 16:01:36 +0800

    add plugin ---  babel-plugin-import

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Icon } from 'antd';
 import { Stage, Layer } from 'react-konva';
 import MainImage from './main-image';
 
@@ -7,14 +8,19 @@ const Index = ({
   image,
 }) => {
   return (
-    <Stage width={800} height={600}>
-      <Layer>
-        <MainImage
-          dispatch={dispatch}
-          image={image}
-        />
-      </Layer>
-    </Stage>
+    <div>
+      <div style={{ width: '100px', height: '100px', background: 'black', color: 'white' }}>
+        <Icon type="delete" />123
+      </div>
+      <Stage width={800} height={600}>
+        <Layer>
+          <MainImage
+            dispatch={dispatch}
+            image={image}
+          />
+        </Layer>
+      </Stage>
+    </div>
   );
 };
 export default Index;
