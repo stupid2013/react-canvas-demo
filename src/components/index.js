32e1267135ae2f31723415d2d9b53c43a70d2ee9
stupid <stupid.du@coloseo.cn>
2018-03-07 16:26:31 +0800

    update

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Icon } from 'antd';
+import { Icon, Button } from 'antd';
 import { Stage, Layer } from 'react-konva';
 import MainImage from './main-image';
 
@@ -9,8 +9,9 @@ const Index = ({
 }) => {
   return (
     <div>
-      <div style={{ width: '100px', height: '100px', background: 'black', color: 'white' }}>
+      <div style={{ width: '100px', height: '100px', background: 'pink', color: 'white' }}>
         <Icon type="delete" />123
+        <Button icon="delete" type="primary">this is a button</Button>
       </div>
       <Stage width={800} height={600}>
         <Layer>
