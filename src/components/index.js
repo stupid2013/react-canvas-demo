8f2d9385e323badc6ec3e7c39e370b47a3c03d13
stupid <stupid.du@coloseo.cn>
2018-03-07 17:19:39 +0800

    update

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Icon, Button } from 'antd';
+import { Icon } from 'antd';
 import { Stage, Layer } from 'react-konva';
 import MainImage from './main-image';
 
@@ -8,19 +8,24 @@ const Index = ({
   image,
 }) => {
   return (
-    <div>
-      <div style={{ width: '100px', height: '100px', background: 'pink', color: 'white' }}>
-        <Icon type="delete" />123
-        <Button icon="delete" type="primary">this is a button</Button>
+    <div style={{ display: 'flex', alignItems: 'flex-start', justifyContent: 'center', marginTop: '48px' }}>
+      <div style={{ fontSize: '24px', lineHeight: '2', width: '1em', marginRight: '32px' }}>
+        <Icon type="plus-circle-o" />
+        <Icon type="minus-circle-o" />
+        <Icon type="file-add" />
+        <Icon type="edit" />
+        <Icon type="reload" />
       </div>
-      <Stage width={800} height={600}>
-        <Layer>
-          <MainImage
-            dispatch={dispatch}
-            image={image}
-          />
-        </Layer>
-      </Stage>
+      <div style={{ border: '1px solid #eee' }}>
+        <Stage width={800} height={600}>
+          <Layer>
+            <MainImage
+              dispatch={dispatch}
+              image={image}
+            />
+          </Layer>
+        </Stage>
+      </div>
     </div>
   );
 };
