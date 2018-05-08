f0941ad65a3ff202eb72441a09ef9454c45c3779
stupid <stupid.du@coloseo.cn>
2018-05-08 18:26:40 +0800

    update

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -2,12 +2,11 @@ import React from 'react';
 import { hashHistory } from 'react-router';
 import styles from './index.css';
 
-// <h1><a onClick={() => hashHistory.push('/hubble')}>调用封装的canvas的组件 demo</a></h1>
-
 export default () => {
   return (
     <div className={styles.wrapper}>
       <h1><a onClick={() => hashHistory.push('/canvas')}>基于konvas和react-canvas的涂鸦测试(demo of konvas)</a></h1>
+      <h1><a onClick={() => hashHistory.push('/hubble')}>调用封装的canvas的组件 demo</a></h1>
     </div>
   );
 };
